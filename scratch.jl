3128529bdc7938ca6e37e0bdc40c1b9060bae509
Neel Smith <dnsmith.neel@gmail.com>
2021-07-05 17:02:32 -0400

    Work on filtering corpus

--- scratch.jl
+++ scratch.jl
@@ -3,14 +3,16 @@ using TextAnalysis
 using CitableText
 using CitableCorpus
 using Unicode
+using HTTP
 
 # Remove all words in stoplist from s
-function trimstr(s, stoplist)
+function trimstr(s, stoplist,  thresh=1)
 	trimmed = []
 	for wd in split(s)
 		if wd in stoplist
-			#skip
-		else
+			#skip if in stop list
+		elseif length(wd) > thresh
+			# skip if too short
 			push!(trimmed, wd)
 		end
 	end
@@ -18,46 +20,13 @@ function trimstr(s, stoplist)
 end
 
 # Normalize text for topic modelling
-function txtforcomment(comment, stoplist)
+function txtforcomment(comment, stoplist, threshhold)
 	lc = Unicode.normalize(comment; stripmark=true) |> lowercase
-	nopunct = replace(lc, r"[:;\.\"]" => "")
-	trimstr(nopunct, stoplist)
+	nopunct = replace(lc, r"[⁑':;\.\"]" => "")
+	trimstr(nopunct, stoplist, threshhold)
 end
 
-using HTTP
-stopwordurl = "https://raw.githubusercontent.com/SophiaSarro/Thesis-Material/master/topic-modelling-data/scholia-stopwords.txt"
-stopdata = String(HTTP.get(stopwordurl).body)
-stoplist = split(stopdata, "\n")
-
-open("stops.txt", "w") do io
-	write(io, stopdata)
-end
-
-
-url = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-normed.cex" 
-c = CitableCorpus.fromurl(CitableTextCorpus, url, "|")
-
-msnodes = begin
-	comments = filter(cn -> endswith(cn.urn.urn, "comment"), c.corpus)
-	commentnodes = map(cn -> CitableNode(cn.urn, txtforcomment(cn.text, stoplist)), comments)
-	filter(cn -> ! isempty(cn.text), commentnodes)
-end
-
-realsrcdocs = txtforcomments(msnodes)
-realdocs = map(s -> StringDocument(s), realsrcdocs)
-tacorpus = TextAnalysis.Corpus(realdocs)
-lex = begin
-	update_inverse_index!(tacorpus)
-	update_lexicon!(tacorpus)
-	lexicon(tacorpus)
-end
-
-
-sparsedtm = DocumentTermMatrix(tacorpus)
-m = dtm(sparsedtm, :dense)
-
-
-# For each row, get index of non-zero values
+# For each row in document-term, get index of non-zero values
 function indexnz(mtrx)
 	allrows = []
     #rcount = 0
@@ -78,55 +47,98 @@ function indexnz(mtrx)
 	allrows
 end
 
-
-
-
-tmdata = indexnz(m)
-
-
-function termcolumn(trm) 
-	findfirst(t -> t == trm, sparsedtm.terms)
+# Find column index for given term
+function termcolumn(trm,sparsem) 
+	findfirst(t -> t == trm, sparsem.terms)
 end
 
-lextsv = begin
+
+stopwordurl = "https://raw.githubusercontent.com/HCMID/scholia-transmission/main/data/stops.txt"
+stopdata = String(HTTP.get(stopwordurl).body)
+stoplist = split(stopdata, "\n")
+
+url = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-normed.cex" 
+c = CitableCorpus.fromurl(CitableTextCorpus, url, "|")
+
+# Prepare list of nodes for TM analysis
+function filterForTm(c::CitableTextCorpus, stopwords, threshhold=1)
+	comments = filter(cn -> endswith(cn.urn.urn, "comment"), c.corpus)
+	commentnodes = map(cn -> CitableNode(cn.urn, txtforcomment(cn.text, stopwords, threshhold)), comments)
+	filter(cn -> ! isempty(cn.text), commentnodes)
+end
+
+#= Toy experiment
+xcomments = filter(cn -> endswith(cn.urn.urn, "comment"), c.corpus)
+tiny = xcomments[1:100]
+tinyc = CitableTextCorpus(tiny)
+filteredtiny = filterForTm(tinyc, stoplist)
+
+tinyc.corpus[5].text
+filteredtiny[5].text
+
+#println(filteredtiny[3].text)
+=#
+
+function tmcorpus(msnodes)
+	srcdocs =  map(cn -> cn.text, msnodes)
+	tadocs = map(s -> StringDocument(s), srcdocs)
+	tacorpus = TextAnalysis.Corpus(tadocs)
+	update_inverse_index!(tacorpus)
+	update_lexicon!(tacorpus)
+	lex = 	lexicon(tacorpus)
+	sparsedtm = DocumentTermMatrix(tacorpus)
+	m = dtm(sparsedtm, :dense)
+	tmdata = indexnz(m)
+
+
 	lines = []
 	for k in keys(lex)
-		push!(lines, string(termcolumn(k),"\t", k))
+		push!(lines, string(termcolumn(k, sparsedtm),"\t", k))
 	end
-	join(lines,"\n") * "\n"
-end
+	lextsv = join(lines,"\n") * "\n"
+	
 
-
-# These are the 2 input files we need to do TM:
-tmdocfile = begin
-	f = tempname()
+	
+	tmdocfile = tempname()
 	datastrings = map(rowv -> join(rowv,","), tmdata)
-	#write(f, join(datastrings,"\n"))
-	#f
-	write("tmdata2.txt",  join(datastrings,"\n"))
-	"tmdata2.txt"
+	open(tmdocfile,"w") do io
+		write(io, join(datastrings,"\n"))
+	end
+	println("TM DOC FILE ", tmdocfile)
+	lexfile = tempname()
+	open(lexfile,"w") do io
+		write(io,lextsv)	
+	end
+	println("LEX FILE ", lexfile)
+
+	tmcorp = readcorp(docfile=tmdocfile, vocabfile=lexfile)
+	TopicModelsVB.fixcorp!(tmcorp, trim=true)
+	tmcorp
 end
 
-lexfile = begin
-	vfile = tempname()
-	#write(vfile,lextsv)
-	#vfile
-	write("lexfile2.tsv", lextsv)
-	"lexfile2.tsv"
-end
+
+msnodes = filterForTm(c, stoplist)
+tmcorp = tmcorpus(msnodes)
+
+
+msnodes[1].text
 
 
 
-tmcorp = readcorp(docfile=tmdocfile, vocabfile=lexfile)
-TopicModelsVB.fixcorp!(tmcorp, trim=true)
-tmcorp.vocab
 
-its = 30
+
+its = 15
 k = 10
 
 model1 = LDA(tmcorp, k)
 train!(model1, iter=its, tol=0)
 
-
 showtopics(model1, cols=k, 20)
+model1.beta
+
+# This is the distribution of vocabulary for a topic
+model1.beta[1,:]
+
+# This is the distribution of topics for a document
+topicdist(model1,1)
 

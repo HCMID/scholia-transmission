8e60a43b7b05ccd025437ef3de65b3efcda20a68
Neel Smith <dnsmith.neel@gmail.com>
2021-07-12 11:41:56 -0400

    Add TM edition

---
+++ utilities/buildtmedition.jl
@@ -0,0 +1,19 @@
+using HmtTopicModels
+using EditionBuilders
+using CitableCorpus
+
+tmbldr = HmtTopicModels.HmtTMBuilder("tm builder", "hmttm")
+xmlurl = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-xml.cex"
+xmlcorpus = CitableCorpus.fromurl(CitableTextCorpus, xmlurl, "|")
+tmfile = "data/topicmodelingedition.cex"
+
+function writeedition(bldr, corpus::CitableTextCorpus, outfile)
+    tmedition = edition(bldr, corpus)
+    tmcex = cex(tmedition)
+
+    open(outfile,"w") do io
+        write(io, tmcex)
+    end
+end
+
+writeedition(tmbldr, xmlcorpus, tmfile)

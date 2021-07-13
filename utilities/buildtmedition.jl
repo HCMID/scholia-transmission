8133c64c6527b6dcc058817d13dda6adae5739f1
Neel Smith <dnsmith.neel@gmail.com>
2021-07-13 07:12:17 -0400

    Write jslda format as well as cex

--- utilities/buildtmedition.jl
+++ utilities/buildtmedition.jl
@@ -1,13 +1,21 @@
+# Build an edition optimized for topic modelling,
+# and write to disk in:
+# - CEX format for a CitableTextCorpus
+# - delimited text for use with jslda
+#
 using HmtTopicModels
 using EditionBuilders
 using CitableCorpus
+using CitableText
 
 tmbldr = HmtTopicModels.HmtTMBuilder("tm builder", "hmttm")
 xmlurl = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-xml.cex"
 xmlcorpus = CitableCorpus.fromurl(CitableTextCorpus, xmlurl, "|")
 tmfile = "data/topicmodelingedition.cex"
 
-function writeedition(bldr, corpus::CitableTextCorpus, outfile)
+
+
+function writeeditions(bldr, corpus::CitableTextCorpus, outfile)
     tmeditionraw = edition(bldr, corpus)
     tmnodes = filter(cn -> ! isempty(cn.text), tmeditionraw.corpus) 
     tmedition = CitableTextCorpus(tmnodes)
@@ -16,6 +24,20 @@ function writeedition(bldr, corpus::CitableTextCorpus, outfile)
     open(outfile,"w") do io
         write(io, tmcex)
     end
+
+
+    scholia = filter(cn -> contains(cn.urn.urn, "tlg5026"), tmnodes)
+    lines = []
+    for sch in scholia
+        wk = workcomponent(sch.urn)
+        parts = split(wk,".")
+        short = string(parts[2],":",passagecomponent(sch.urn))
+        push!(lines, string(short,"\tscholion\t", sch.text))
+    end
+    jslda = join(lines,"\n") * "\n"
+    open("data/scholia-jslda.tsv", "w") do io
+        write(io, jslda)
+    end
 end
 
-writeedition(tmbldr, xmlcorpus, tmfile)
+writeeditions(tmbldr, xmlcorpus, tmfile)

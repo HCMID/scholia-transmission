3d029547cd2f55362be484419c43e32e41b9467f
Neel Smith <dnsmith.neel@gmail.com>
2021-07-15 12:51:42 -0400

    select scholia only for topic modelling

--- utilities/buildtmedition.jl
+++ utilities/buildtmedition.jl
@@ -7,13 +7,34 @@ using HmtTopicModels
 using EditionBuilders
 using CitableCorpus
 using CitableText
+using CitableObject
 
 tmbldr = HmtTopicModels.HmtTMBuilder("tm builder", "hmttm")
 xmlurl = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-xml.cex"
 xmlcorpus = CitableCorpus.fromurl(CitableTextCorpus, xmlurl, "|")
+scholia = filter(cn -> endswith(cn.urn.urn, "comment"), xmlcorpus.corpus) |> CitableTextCorpus
 tmfile = "data/topicmodelingedition.cex"
+tmcorp = tmcorpus(xmlcorpus)
 
-
+#=
+function tidyurns(s)
+    tkns = split(s)
+    tidier = []
+    for t in tkns
+        if startswith(t, "urn:cite2:hmt:pers")
+            u = Cite2Urn(t)
+            rawnumeral = replace(objectcomponent(u), "pers" => "")
+            #=
+            shorter = string("pers_", objectcomponent(u))
+            push!(tidier, shorter)
+            =#
+            push!(tidier, rawnumeral)
+        else
+            push!(tidier, t)
+        end
+    end
+    join(tidier," ")
+end
 
 function writeeditions(bldr, corpus::CitableTextCorpus, outfile)
     tmeditionraw = edition(bldr, corpus)
@@ -32,7 +53,7 @@ function writeeditions(bldr, corpus::CitableTextCorpus, outfile)
         wk = workcomponent(sch.urn)
         parts = split(wk,".")
         short = string(parts[2],":",passagecomponent(sch.urn))
-        push!(lines, string(short,"\tscholion\t", sch.text))
+        push!(lines, string(short,"\tscholion\t", tidyurns(sch.text)))
     end
     jslda = join(lines,"\n") * "\n"
     open("data/scholia-jslda.tsv", "w") do io
@@ -41,3 +62,4 @@ function writeeditions(bldr, corpus::CitableTextCorpus, outfile)
 end
 
 writeeditions(tmbldr, xmlcorpus, tmfile)
+=#

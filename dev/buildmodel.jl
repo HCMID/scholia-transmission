3de0c5dd9fca7039d6019fd4a0a76535a2eafb8b
Neel Smith <dnsmith.neel@gmail.com>
2021-07-26 10:12:33 -0400

    Add notebook to build composite model

--- dev/buildmodel.jl
+++ dev/buildmodel.jl
@@ -4,65 +4,29 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 2511b4d8-eb45-11eb-1fe3-134074818861
+# ╔═╡ 44b14430-ee1b-11eb-171c-0f0798f9ecb2
 begin
-	using CitableCorpus
-	using CitableText
-	using PlutoUI
+	using CSV
+	using HTTP
+	using DataFrames
 	
-	md"""
-Notebook version: **0.1.0**
-"""	
+	md"Notebook version:  unreleased"
 end
 
-# ╔═╡ fa4c1d48-010d-43ff-a2a1-9c7e2634f8f6
-md"""> # Compute size of scholia
->
-> Compute word count for each scholion.
-> Include option to download counts as a delimited-text file.
-"""
-
-# ╔═╡ 254779c9-50ef-47d7-bfb3-b43cd878852c
-md"> Download text corpus and filter to select scholia comments"
-
-# ╔═╡ 21ecbf83-24b2-4e62-881e-94290ada4ade
-url = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-normed.cex"
-
-# ╔═╡ d6a3cbb0-e41c-4d7e-9cdd-a13285925c31
-c = CitableCorpus.fromurl(CitableTextCorpus, url, "|")
-
-# ╔═╡ 2dc099fa-6f02-41ef-9906-228248da7ef3
-# Comment nodes from all HMT scholia
-scholia = filter(cn -> occursin("comment", cn.urn.urn),  c.corpus)
-
-# ╔═╡ 6ad5da6d-efb6-45b5-b88c-466775d89676
-# Create a list pairing URNs with counts of "words" in each scholion
-sizes = begin
-	sizepairs = []
-	for cn in scholia
-		wdcount = split(cn.text) |> length
-		push!(sizepairs, string(cn.urn.urn, "|", wdcount))
-	end
-	sizepairs
-end
-
-# ╔═╡ d6849a9f-51ca-4a98-9ef9-9faa031ce619
-begin
-	delimited = join(sizes,"\n")
-	md"- Download delimited file $(DownloadButton(delimited,\"scholialengths.cex\"))"
-end
+# ╔═╡ e52c6188-82ba-4d55-ab2a-d25ce78a2b9e
+md"> # Build composite model of scholia"
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
-CitableCorpus = "cf5ac11a-93ef-4a1a-97a3-f6af101603b5"
-CitableText = "41e66566-473b-49d4-85b7-da83b66615d8"
-PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+CSV = "336ed68f-0bac-5ca0-87d4-7b16caf5d00b"
+DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+HTTP = "cd3eb016-35fb-5094-929b-558a96fad6f3"
 
 [compat]
-CitableCorpus = "~0.3.0"
-CitableText = "~0.9.0"
-PlutoUI = "~0.7.9"
+CSV = "~0.8.5"
+DataFrames = "~1.2.1"
+HTTP = "~0.9.12"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -78,41 +42,11 @@ uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
 [[Base64]]
 uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
 
-[[BenchmarkTools]]
-deps = ["JSON", "Logging", "Printf", "Statistics", "UUIDs"]
-git-tree-sha1 = "9e62e66db34540a0c919d72172cc2f642ac71260"
-uuid = "6e4b80f9-dd63-53aa-95a3-0cdb28fa8baf"
-version = "0.5.0"
-
-[[BinaryProvider]]
-deps = ["Libdl", "Logging", "SHA"]
-git-tree-sha1 = "ecdec412a9abc8db54c0efc5548c64dfce072058"
-uuid = "b99e7846-7c00-51b0-8f62-c81ae34c0232"
-version = "0.5.10"
-
 [[CSV]]
 deps = ["Dates", "Mmap", "Parsers", "PooledArrays", "SentinelArrays", "Tables", "Unicode"]
 git-tree-sha1 = "b83aa3f513be680454437a0eee21001607e5d983"
 uuid = "336ed68f-0bac-5ca0-87d4-7b16caf5d00b"
 version = "0.8.5"
-
-[[CitableBase]]
-deps = ["DocStringExtensions", "Documenter", "Test"]
-git-tree-sha1 = "e1edbddb151b18f8290b8f19e4310c369b01c049"
-uuid = "d6f014bd-995c-41bd-9893-703339864534"
-version = "1.2.2"
-
-[[CitableCorpus]]
-deps = ["CSV", "CitableText", "DataFrames", "DocStringExtensions", "Documenter", "HTTP", "Test", "TextAnalysis"]
-git-tree-sha1 = "a0adc4d10424fa4e884cfaecdb4ee9f8a019fdcd"
-uuid = "cf5ac11a-93ef-4a1a-97a3-f6af101603b5"
-version = "0.3.0"
-
-[[CitableText]]
-deps = ["BenchmarkTools", "CitableBase", "DocStringExtensions", "Documenter", "Test"]
-git-tree-sha1 = "3d95c0ceea520fae5248a6842026b99d6ca23356"
-uuid = "41e66566-473b-49d4-85b7-da83b66615d8"
-version = "0.9.0"
 
 [[Compat]]
 deps = ["Base64", "Dates", "DelimitedFiles", "Distributed", "InteractiveUtils", "LibGit2", "Libdl", "LinearAlgebra", "Markdown", "Mmap", "Pkg", "Printf", "REPL", "Random", "SHA", "Serialization", "SharedArrays", "Sockets", "SparseArrays", "Statistics", "Test", "UUIDs", "Unicode"]
@@ -129,12 +63,6 @@ version = "4.0.4"
 git-tree-sha1 = "ee400abb2298bd13bfc3df1c412ed228061a2385"
 uuid = "9a962f9c-6df0-11e9-0e5d-c546b8b5ee8a"
 version = "1.7.0"
-
-[[DataDeps]]
-deps = ["BinaryProvider", "HTTP", "Libdl", "Reexport", "SHA", "p7zip_jll"]
-git-tree-sha1 = "4f0e41ff461d42cfc62ff0de4f1cd44c6e6b3771"
-uuid = "124859b0-ceae-595e-8997-d05f6a7a8dfe"
-version = "0.7.7"
 
 [[DataFrames]]
 deps = ["Compat", "DataAPI", "Future", "InvertedIndices", "IteratorInterfaceExtensions", "LinearAlgebra", "Markdown", "Missings", "PooledArrays", "PrettyTables", "Printf", "REPL", "Reexport", "SortingAlgorithms", "Statistics", "TableTraits", "Tables", "Unicode"]
@@ -165,18 +93,6 @@ uuid = "8bb1440f-4735-579b-a4ab-409b98df4dab"
 deps = ["Random", "Serialization", "Sockets"]
 uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
 
-[[DocStringExtensions]]
-deps = ["LibGit2"]
-git-tree-sha1 = "a32185f5428d3986f47c2ab78b1f216d5e6cc96f"
-uuid = "ffbed154-4ef7-542d-bbb7-c09d3a79fcae"
-version = "0.8.5"
-
-[[Documenter]]
-deps = ["Base64", "Dates", "DocStringExtensions", "IOCapture", "InteractiveUtils", "JSON", "LibGit2", "Logging", "Markdown", "REPL", "Test", "Unicode"]
-git-tree-sha1 = "3ebb967819b284dc1e3c0422229b58a40a255649"
-uuid = "e30172f5-a6a5-5a46-863b-614d45cd2de4"
-version = "0.26.3"
-
 [[Downloads]]
 deps = ["ArgTools", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
@@ -191,23 +107,11 @@ version = "0.4.2"
 deps = ["Random"]
 uuid = "9fa8497b-333b-5362-9e8d-4d0656e87820"
 
-[[HTML_Entities]]
-deps = ["StrTables"]
-git-tree-sha1 = "c4144ed3bc5f67f595622ad03c0e39fa6c70ccc7"
-uuid = "7693890a-d069-55fe-a829-b4a6d304f0ee"
-version = "1.0.1"
-
 [[HTTP]]
 deps = ["Base64", "Dates", "IniFile", "Logging", "MbedTLS", "NetworkOptions", "Sockets", "URIs"]
 git-tree-sha1 = "c6a1fff2fd4b1da29d3dccaffb1e1001244d844e"
 uuid = "cd3eb016-35fb-5094-929b-558a96fad6f3"
 version = "0.9.12"
-
-[[IOCapture]]
-deps = ["Logging"]
-git-tree-sha1 = "377252859f740c217b936cebcd918a44f9b53b59"
-uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
-version = "0.1.1"
 
 [[IniFile]]
 deps = ["Test"]
@@ -229,18 +133,6 @@ version = "1.0.0"
 git-tree-sha1 = "a3f24677c21f5bbe9d2a714f95dcd58337fb2856"
 uuid = "82899510-4779-5014-852e-03e436cf321d"
 version = "1.0.0"
-
-[[JSON]]
-deps = ["Dates", "Mmap", "Parsers", "Unicode"]
-git-tree-sha1 = "81690084b6198a2e1da36fcfda16eeca9f9f24e4"
-uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
-version = "0.21.1"
-
-[[Languages]]
-deps = ["InteractiveUtils", "JSON"]
-git-tree-sha1 = "b1a564061268ccc3f3397ac0982983a657d4dcb8"
-uuid = "8ef0a80b-9436-5d2c-a485-80b904378c43"
-version = "0.4.3"
 
 [[LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
@@ -312,12 +204,6 @@ version = "1.1.0"
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
 
-[[PlutoUI]]
-deps = ["Base64", "Dates", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "Suppressor"]
-git-tree-sha1 = "44e225d5837e2a2345e69a1d1e01ac2443ff9fcb"
-uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-version = "0.7.9"
-
 [[PooledArrays]]
 deps = ["DataAPI", "Future"]
 git-tree-sha1 = "cde4ce9d6f33219465b55162811d8de8139c0414"
@@ -333,12 +219,6 @@ version = "1.1.0"
 [[Printf]]
 deps = ["Unicode"]
 uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
-
-[[ProgressMeter]]
-deps = ["Distributed", "Printf"]
-git-tree-sha1 = "afadeba63d90ff223a6a48d2009434ecee2ec9e8"
-uuid = "92933f4c-e287-5a05-a399-4b506db050ca"
-version = "1.7.1"
 
 [[REPL]]
 deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
@@ -369,18 +249,6 @@ uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
 deps = ["Distributed", "Mmap", "Random", "Serialization"]
 uuid = "1a1011a3-84de-559e-8e89-a11a2f7dc383"
 
-[[Snowball]]
-deps = ["Languages", "Snowball_jll", "WordTokenizers"]
-git-tree-sha1 = "d38c1ff8a2fca7b1c65a51457dabebef28052399"
-uuid = "fb8f903a-0164-4e73-9ffe-431110250c3b"
-version = "0.1.0"
-
-[[Snowball_jll]]
-deps = ["Libdl", "Pkg"]
-git-tree-sha1 = "35031519df40fbf0d4a6d2faae4f00e117b0ad11"
-uuid = "88f46535-a3c0-54f4-998e-4320a1339f51"
-version = "2.0.0+0"
-
 [[Sockets]]
 uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
 
@@ -397,28 +265,6 @@ uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
 [[Statistics]]
 deps = ["LinearAlgebra", "SparseArrays"]
 uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
-
-[[StatsAPI]]
-git-tree-sha1 = "1958272568dc176a1d881acb797beb909c785510"
-uuid = "82ae8749-77ed-4fe6-ae5f-f523153014b0"
-version = "1.0.0"
-
-[[StatsBase]]
-deps = ["DataAPI", "DataStructures", "LinearAlgebra", "Missings", "Printf", "Random", "SortingAlgorithms", "SparseArrays", "Statistics", "StatsAPI"]
-git-tree-sha1 = "2f6792d523d7448bbe2fec99eca9218f06cc746d"
-uuid = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
-version = "0.33.8"
-
-[[StrTables]]
-deps = ["Dates"]
-git-tree-sha1 = "5998faae8c6308acc25c25896562a1e66a3bb038"
-uuid = "9700d1a9-a7c8-5760-9816-a99fda30bb8f"
-version = "1.0.1"
-
-[[Suppressor]]
-git-tree-sha1 = "a819d77f31f83e5792a76081eee1ea6342ab8787"
-uuid = "fd094767-a336-5f1f-9728-57cf17d0bbfb"
-version = "0.2.0"
 
 [[TOML]]
 deps = ["Dates"]
@@ -444,12 +290,6 @@ uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
 uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 
-[[TextAnalysis]]
-deps = ["DataStructures", "DelimitedFiles", "JSON", "Languages", "LinearAlgebra", "Printf", "ProgressMeter", "Random", "Serialization", "Snowball", "SparseArrays", "Statistics", "StatsBase", "Tables", "WordTokenizers"]
-git-tree-sha1 = "bc85e54209c30e69e1925460ec0257a916683f59"
-uuid = "a2db99b7-8b79-58f8-94bf-bbc811eef33d"
-version = "0.7.3"
-
 [[URIs]]
 git-tree-sha1 = "97bbe755a53fe859669cd907f2d96aee8d2c1355"
 uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
@@ -461,12 +301,6 @@ uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
 
 [[Unicode]]
 uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
-
-[[WordTokenizers]]
-deps = ["DataDeps", "HTML_Entities", "StrTables", "Unicode"]
-git-tree-sha1 = "01dd4068c638da2431269f49a5964bf42ff6c9d2"
-uuid = "796a5d58-b03d-544a-977e-18100b691f6e"
-version = "0.5.6"
 
 [[Zlib_jll]]
 deps = ["Libdl"]
@@ -482,13 +316,7 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 """
 
 # ╔═╡ Cell order:
-# ╟─2511b4d8-eb45-11eb-1fe3-134074818861
-# ╟─fa4c1d48-010d-43ff-a2a1-9c7e2634f8f6
-# ╟─d6849a9f-51ca-4a98-9ef9-9faa031ce619
-# ╟─6ad5da6d-efb6-45b5-b88c-466775d89676
-# ╟─254779c9-50ef-47d7-bfb3-b43cd878852c
-# ╟─21ecbf83-24b2-4e62-881e-94290ada4ade
-# ╟─d6a3cbb0-e41c-4d7e-9cdd-a13285925c31
-# ╟─2dc099fa-6f02-41ef-9906-228248da7ef3
+# ╟─44b14430-ee1b-11eb-171c-0f0798f9ecb2
+# ╟─e52c6188-82ba-4d55-ab2a-d25ce78a2b9e
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
